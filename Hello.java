6848e4f86365dde038dfc812c5f8359eb12a9085
kiyongee2 <kiyongee2@gmail.com>
2020-05-31 08:23:29 +0900

    helloName() def

--- Hello.java
+++ Hello.java
@@ -4,10 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
+		helloName("Tom");
+		helloName("Jerry");
 	}
 	
 	public static void hello() {
-		System.out.println("æ»≥Á«œººø‰~");
+		System.out.println("Hello~");
+	}
+	
+	public static void helloName(String name) {
+		System.out.println("Hello~ " + name);
 	}
 
 }
