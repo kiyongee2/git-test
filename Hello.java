734b613cc6af601e0e0ad3df258c183d8f2fc3bc
kiyongee2 <kiyongee2@gmail.com>
2020-05-31 08:12:00 +0900

    [hello()]

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package github;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		hello();
+	}
+	
+	public static void hello() {
+		System.out.println("æ»≥Á«œººø‰~");
+	}
+
+}
